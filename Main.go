b2d7740a67e5b57013374b09c26a82d5778799a5
kiddynamo <njenan@gmail.com>
2015-12-25 21:20:29 -0800

    Adding json write/read support, notes, .gitignore.

---
+++ Main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"math/rand"
+	"strconv"
+	"io/ioutil"
+	"io"
+	"strings"
+	"os"
+)
+
+var (
+	GET_HANDLER = "GET"
+	POST_HANDLER = "POST"
+	BASE_DIR = "/Users/kiddynamo/git/gdg-go-presentation/"
+	store map[int]map[string]interface{} = make(map[int]map[string]interface{})
+	requestHandlers map[string]handler = make(map[string]handler)
+)
+
+type handler func(w http.ResponseWriter, r *http.Request)
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	handler := requestHandlers[r.Method]
+	handler(w, r)
+}
+
+func readBodyToJson(body io.ReadCloser) (map[string]interface{}, error) {
+	var out map[string]interface{}
+	data, err := ioutil.ReadAll(body)
+
+	if err == nil {
+		err = json.Unmarshal(data, &out)
+
+		if err == nil {
+			id := rand.Int()
+			out["__id"] = id
+
+			store[id] = out
+		}
+	}
+
+	return out, err
+}
+
+func writeJsonToDisk(jsonBlob map[string]interface{}) error {
+	id := strconv.Itoa(jsonBlob["__id"].(int))
+	file, err := os.Create(BASE_DIR + id)
+
+	if err == nil {
+		var jsonAsString []byte
+		jsonAsString, err = json.Marshal(jsonBlob)
+
+		if err == nil {
+			_, err = file.Write(jsonAsString)
+		}
+	}
+
+	return err
+}
+
+func getPacketByKey(key string) (map[string]interface{}, error) {
+	var out map[string]interface{}
+	filepath := BASE_DIR + key
+	bytes, err := ioutil.ReadFile(filepath)
+
+	if err == nil {
+		err = json.Unmarshal(bytes, &out)
+	}
+
+	return out, err
+}
+
+func setupRoutes() {
+	requestHandlers[GET_HANDLER] = func(w http.ResponseWriter, r *http.Request) {
+		key := strings.TrimLeft(r.URL.Path, "/")
+		var write string
+
+		packet, err := getPacketByKey(key)
+
+		if err == nil {
+			var bytes []byte
+			bytes, err = json.Marshal(packet)
+
+			if err == nil {
+				write = string(bytes)
+			}
+		}
+
+		if err != nil {
+			write = err.Error()
+		}
+
+		fmt.Fprintf(w, write)
+	}
+
+	requestHandlers[POST_HANDLER] = func(w http.ResponseWriter, r *http.Request) {
+		var write string
+
+		out, err := readBodyToJson(r.Body)
+
+		if err == nil {
+			err = writeJsonToDisk(out)
+
+			if err == nil {
+				var toret []byte
+				toret, err = json.Marshal(out)
+				write = string(toret)
+			}
+		}
+
+		if err != nil {
+			write = err.Error()
+		}
+
+		fmt.Fprintf(w, write)
+	}
+}
+
+func main() {
+	setupRoutes()
+	http.HandleFunc("/", handle)
+
+	port := "8080"
+	fmt.Println("Webserver listening at port: " + port)
+	http.ListenAndServe(":" + port, nil)
+}
---
+++ NotesToMyself.md
@@ -0,0 +1,25 @@
+comment on interesting features of the language
+
+'clean' syntax, no parens around if-else or loops
+while loop is just a for loop with no args
+
+
+functions
+- defer functions
+  - stacking
+  - this is go's 'finally' block
+- Closures
+
+
+Pointers
+- They exist, but no pointer arithmetic
+
+
+Dependency management
+- Still in its infancy- isn't as full fledged as something like maven or npm
+- 'Vendoring' is a possible solution (copying source into repository)
+- Do demo of Vendoring this is what google does [source](http://jbeckwith.com/2015/05/29/dependency-management-go/)
+
+
+General notes
+- No type coercion, must be explicit
