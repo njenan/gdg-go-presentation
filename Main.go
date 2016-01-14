6c7f64ce7d0e04f46a8024a988ac41f4f2b01e44
kiddynamo <njenan@gmail.com>
2016-01-13 20:51:16 -0800

    Concurrently assembled toret

--- Main.go
+++ Main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"strconv"
 	"math/rand"
+	"errors"
 )
 
 var store map[int]map[string]interface{}
@@ -14,38 +15,64 @@ func init() {
 	store = make(map[int]map[string]interface{})
 }
 
-func get(w http.ResponseWriter, r *http.Request) {
+type MethodHandler func(w http.ResponseWriter, r *http.Request) ([]byte, error)
+
+func get(w http.ResponseWriter, r *http.Request) ([]byte, error) {
 	stringId := r.URL.Path[1:]
+
+	if stringId == "" {
+		return search(w, r)
+	}
 
 	var id int
 	var err error
 
 	if id, err = strconv.Atoi(stringId); err != nil {
-		w.Write([]byte(err.Error()))
-		return
+		return nil, errors.New("Key was not a number")
 	}
 
 	jsonObj := store[id]
 
 	if jsonObj == nil {
-		w.Write([]byte("Object does not exist"))
-		return
+		return nil, errors.New("Document does not exist")
 	}
 
 	var bytes []byte
 	if bytes, err = json.Marshal(jsonObj); err != nil {
-		w.Write([]byte("Could not marshal object"))
+		return nil, errors.New("Could not marshal object")
 	}
 
-	w.Write(bytes)
+	return bytes, nil
 }
 
-func post(w http.ResponseWriter, r *http.Request) {
+func search(w http.ResponseWriter, r *http.Request) ([]byte, error) {
+	size := len(store)
+	allJsons := make([]map[string]interface{}, size)
+
+	channel := make(chan map[string]interface{})
+
+	for key, _ := range store {
+		go func(key int, channel chan map[string]interface{}) {
+			channel <- store[key]
+		}(key, channel)
+	}
+
+	for i := 0; i < size; i ++ {
+		allJsons = append(allJsons, <- channel)
+	}
+
+	if bytes, err := json.Marshal(allJsons); err != nil {
+		return nil, errors.New("Could not marshal object")
+	} else {
+		return bytes, nil
+	}
+}
+
+func post(w http.ResponseWriter, r *http.Request) ([]byte, error) {
 	var jsonBlob map[string]interface{}
 
 	if err := json.NewDecoder(r.Body).Decode(&jsonBlob); err != nil {
-		w.Write([]byte(err.Error()))
-		return
+		return nil, errors.New("Could not parse body as json")
 	}
 
 	id := rand.Intn(1000000)
@@ -53,21 +80,28 @@ func post(w http.ResponseWriter, r *http.Request) {
 	store[id] = jsonBlob
 
 	if bytes, err := json.Marshal(jsonBlob); err != nil {
-		w.Write([]byte(err.Error()))
+		return nil, errors.New("Could not marshal json")
 	} else {
-		w.Write(bytes)
+		return bytes, nil
 	}
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
+		var handler MethodHandler
+
 		if r.Method == "GET" {
-			get(w, r)
+			handler = get
 		} else if r.Method == "POST" {
-			post(w, r)
+			handler = post
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		if bytes, err := handler(w, r); err != nil {
+			w.Write([]byte(err.Error()))
 		} else {
-			w.Write([]byte("Something went wrong"))
+			w.Write(bytes)
 		}
 	})
 
