8622838d644b99171b5f8577bca5e78d12a52ada
kiddynamo <njenan@gmail.com>
2017-12-21 11:13:14 -0700

    Basic function use, defer, panic, recover, args

--- Main.go
+++ Main.go
@@ -4,41 +4,26 @@ import (
 	"fmt"
 )
 
+func toDefer(i int) {
+	fmt.Printf("Defer func execute count = %v\n", i)
+}
+
+func badFunc() {
+	panic("Something went wrong")
+}
+
 func main() {
-	// var x bool
-	// x = false
+	defer toDefer(1)
+	defer toDefer(2)
+	defer toDefer(3)
+	//defer func() {
+	//	fmt.Printf("Recovered from '%v'\n", recover())
+	//}()
 
-	x := false
+	fmt.Println("Main function body")
 
-	if x {
-		fmt.Println("X is true")
-	} else {
-		fmt.Println("X is false")
-	}
+	badFunc()
+	recover()
 
-	for i := 0; i < 5; i++ {
-		fmt.Println(i)
-	}
-
-	var z int
-	z = 100
-	for {
-		z++
-		fmt.Println(z)
-
-		if z > 104 {
-			break
-		}
-	}
-
-	cont := true
-	for cont {
-		fmt.Println("continue")
-		cont = false
-	}
-
-	numbers := []int{9, 8, 7, 6, 5}
-	for index, val := range numbers {
-		fmt.Printf("index=%v value=%v\n", index, val)
-	}
+	fmt.Println("After bad function")
 }
