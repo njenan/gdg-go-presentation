9866d24b7fc359a5d8b3c08d30f314fc1e77f094
kiddynamo <njenan@gmail.com>
2017-12-21 16:43:22 -0700

    Example of oop in go

--- Main.go
+++ Main.go
@@ -2,41 +2,32 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
 
-func print(i int, wg *sync.WaitGroup) {
-	time.Sleep(time.Duration(i) * time.Second)
-	fmt.Println(i)
-	wg.Done()
+type Animal interface {
+	call()
 }
 
-func calcSquare(i int, ch chan int) {
-	ch <- i * i
+type Cat struct {
+}
+
+type Dog struct {
+}
+
+func (dog *Dog) call() {
+	fmt.Println("Bark")
+}
+
+func (cat *Cat) call() {
+	fmt.Println("Meow")
 }
 
 func main() {
-	/*
-		var wg sync.WaitGroup
-		wg.Add(3)
+	var animal Animal
 
-		go print(1, &wg)
-		go print(2, &wg)
-		go print(3, &wg)
+	animal = &Dog{}
+	animal.call()
 
-		wg.Wait()
-
-		fmt.Println("Done")
-	*/
-
-	channel := make(chan int)
-
-	go calcSquare(9, channel)
-	go calcSquare(15, channel)
-	go calcSquare(31, channel)
-	x, y, z := <-channel, <-channel, <-channel
-
-	fmt.Printf("%v, %v, %v\n", x, y, z)
-
+	animal = &Cat{}
+	animal.call()
 }
